f6f0a2f6fb40265c17ad7f9757f74f3130490647
Stefano Agostini <stefano.agostini93@gmail.com>
2021-04-04 10:41:57 +0200

    model.py file documented

--- model.py
+++ model.py
@@ -1,11 +1,6 @@
-import logging
 import os
 
 from pySmartDL import SmartDL
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='(%(threadName)-10s) %(message)s',
-                    )
 
 class Download:
     def __init__(self, url, fileName, destPath):  
@@ -18,11 +13,13 @@ class Download:
         self.status = None
         self.progressStatus = 0
 
-    
+
+    # start the download
     def start(self):
         self.download.start(blocking=False)
         
     
+    # pause the download
     def pause(self):
         if (self.download.get_status() == "downloading"):
             self.download.pause()
@@ -30,22 +27,22 @@ class Download:
             self.download.resume()
     
 
+    # cancel the download
     def stop(self):
         self.download.resume()
         self.download.stop()
         self.downloadCancelled = True
 
 
+    # return the progress status and a message that explain if the download is finished, cancelled or in pause
     def getInfo(self):
         self.progressStatus = self.download.get_progress()*100;
-        logging.debug("Progress: " + str(self.progressStatus))
         self.status = self.download.get_status()
         if (not self.downloadCancelled):
             if self.download.isFinished():
                 if self.download.isSuccessful():
                     if self.finished == False:
                         self.finished = True
-                        logging.debug("downloaded file to '%s'" % self.download.get_dest())
                         if (self.fileName is not None):
                             self.path = self.destPath +"/"+ self.fileName
                         else:
@@ -56,11 +53,13 @@ class Download:
         return self.progressStatus, self.status
 
     
+    # utility method to extract the name of the file from the url
     def getFilenameFromUrl(self):
         filename = self.url[self.url.rfind("/") + 1:]
         return str(filename)
     
     
+    # save the file from the temp folder to destination path
     def renameFile(self, old, new):
         os.rename(old , new)
     
